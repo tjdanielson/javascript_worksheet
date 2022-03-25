fc19b2f27e255d6d8bfcdcce68c40d1885f1c267
tjdanielson <tessajdanielson@gmail.com>
2022-03-25 13:31:41 -0500

    Completed problem four.

--- worksheet.js
+++ worksheet.js
@@ -21,18 +21,35 @@
 // Problem Three Conditionals - Meals Breakfast - banana toast lunch - salad dinner - pasta
 
 
-function generateMeal() {
-    let timeOfDay = 1300
-    let meal = ''
-    if (timeOfDay < 1200) {
-        meal = 'Peanut Butter Banana Toast'
-    } else if (timeOfDay >= 1200 && timeOfDay <= 1700) {
-        meal = 'Salad'
-    } else if (timeOfDay > 1700) {
-        meal = 'Pasta'
+// function generateMeal() {
+//     let timeOfDay = 1300
+//     let meal = ''
+//     if (timeOfDay < 1200) {
+//         meal = 'Peanut Butter Banana Toast'
+//     } else if (timeOfDay >= 1200 && timeOfDay <= 1700) {
+//         meal = 'Salad'
+//     } else if (timeOfDay > 1700) {
+//         meal = 'Pasta'
+//     }
+//     console.log(meal)
+// }
+
+// generateMeal()
+
+// Problem Four Conditionals - Random Number
+
+let randomNumber = Math.floor(Math.random() * 10);
+
+function randomBand(randomNumber) {
+    if (randomNumber >= 0 && randomNumber <= 2) {
+        console.log('Beatles')
+    } else if (randomNumber >= 3 && randomNumber <= 5) {
+        console.log('Stones')
+    } else if (randomNumber >= 6 && randomNumber <= 8) {
+        console.log('Floyd')
+    } else if (randomNumber >= 9 && randomNumber <= 10) {
+        console.log('Hendrix')
     }
-    console.log(meal)
 }
 
-generateMeal()
-
+randomBand(randomNumber);
