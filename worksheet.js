36ad614c4bd07be923f0af8d3c207f47e6cac6ad
tjdanielson <tessajdanielson@gmail.com>
2022-03-25 12:46:29 -0500

    Completed problem 2.

--- worksheet.js
+++ worksheet.js
@@ -1,10 +1,16 @@
 // Problem One: Day of the Week Variable
 
-let dayOfWeek = 'Monday';
+// let dayOfWeek = 'Monday';
 
-console.log(dayOfWeek)
+// console.log(dayOfWeek)
 
-dayOfWeek = 'Friday';
+// dayOfWeek = 'Friday';
 
-console.log(`I can't wait for ${dayOfWeek}`)
+// console.log(`I can't wait for ${dayOfWeek}`)
 
+// Problem Two: User Input
+
+animalInput = prompt('What is your favorite animal?')
+colorInput = prompt('What is your favorite color?')
+
+console.log(`I've never seen a ${colorInput} ${animalInput}!`)
